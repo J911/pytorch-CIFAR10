f6164c62c0c77350e15a862079b5c4d9c7bf84cb
Jaemin <jaemin.lee.email@gmail.com>
2019-09-17 10:48:48 +0900

    Update train.py

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ for epoch in range(2):
         optimizer.step()
 
         running_loss += loss.item()
-        if i % 2000 == 1999: 
+        if i % 100 == 0: 
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
