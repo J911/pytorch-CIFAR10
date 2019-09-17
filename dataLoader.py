8e06e802f6c7f8c06d0be81bc41230686170664c
Jaemin <jaemin.lee.email@gmail.com>
2019-09-17 10:55:42 +0900

    Update dataLoader.py

--- dataLoader.py
+++ dataLoader.py
@@ -9,12 +9,12 @@ def dataLoader():
 
     trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=256,
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=64,
                                             shuffle=True, num_workers=1)
 
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                         download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=256,
+    testloader = torch.utils.data.DataLoader(testset, batch_size=64,
                                             shuffle=False, num_workers=1)
 
     classes = ('plane', 'car', 'bird', 'cat',
